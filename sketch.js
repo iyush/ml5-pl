5d4bdd4e2facce9c87737da9b87167cc30a272c3
Aayush Acharya <aayushsharma889@gmail.com>
2020-01-19 00:41:50 +0100

    added click to upload button

--- sketch.js
+++ sketch.js
@@ -7,16 +7,37 @@ let canvasWidth, canvasHeight;
 
 function setup() {
     imgField = select('#dropzone');
+    imgField.mousePressed(() => {
+        let input = document.createElement('input');
+        input.type = 'file';
+        input.click();
+        let file;
+        input.onchange = e => {
+            file = e.target.files[0];
+            let reader = new FileReader()
+            reader.readAsDataURL(file);
+            reader.onload = readerEvent => {
+                readerEvent.data = readerEvent.target.result;
+                gotFile(readerEvent);
+            }
+        }
+    });
+
     imgField.dragOver(
         () => imgField.style('background-color', '#ccc')
     );
+
     imgField.dragLeave(
         () => imgField.style('background-color', '#fff')
     );
+
     imgField.drop(gotFile,
         () => imgField.style('background-color', '#fff')
     );
+}
 
+function msgDisplay(txt) {
+    document.getElementById("classify").innerHTML = txt;
 }
 
 function gotFile(file) {
@@ -25,25 +46,25 @@ function gotFile(file) {
         '',
         '',
         () => {
-            console.log(img.width, img.height);
+            msgDisplay("Classifying......... (Please Wait)");
             let ratio = (img.width / img.height);
             canvasHeight = 300;
             canvasWidth = canvasHeight * ratio
-            
+
             c = createCanvas(canvasWidth, canvasHeight);
             background(0);
             image(img, 0, 0, width, height)
             c.parent('sketch-holder');
+            ml5.
+                imageClassifier("MobileNet")
+                .ready
+                .then((classifier) => {
+                    classifier.classify(img, numberOfClassifications, gotResults)
+                })
+                .catch((err) => console.error(err));
         }
     );
     img.hide();
-    ml5.
-        imageClassifier("MobileNet")
-        .ready
-        .then((classifier) => {
-            classifier.classify(img, numberOfClassifications, gotResults)
-        })
-        .catch((err) => console.error(err));
 }
 
 function gotResults(error, data) {
@@ -51,7 +72,6 @@ function gotResults(error, data) {
     else {
         let label = data[0].label
         let conf = data[0].confidence * 100
-        document.getElementById("nameOfClassification").innerHTML = '<b>Label: </b>' + label + "<br>";
-        document.getElementById("probofClassification").innerHTML = "<b>Confidence Score: </b>" + conf;
+        msgDisplay('<b>Label: </b>' + label + "<br>" + "<b>Confidence Score: </b>" + conf);
     }
 }
